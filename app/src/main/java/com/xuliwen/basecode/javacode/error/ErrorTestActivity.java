2bee46a422bf95aec7f7ca8e3f4de27d19e16e76
guangmomo <1853959636@qq.com>
2017-10-28 09:46:06 +0800

    [modify] 忽略不需要的文件； 添加java关于Error类的基础

---
+++ app/src/main/java/com/xuliwen/basecode/javacode/error/ErrorTestActivity.java
@@ -0,0 +1,15 @@
+package com.xuliwen.basecode.javacode.error;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xuliwen.basecode.R;
+
+public class ErrorTestActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_error_test);
+	}
+}
